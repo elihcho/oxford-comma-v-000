4e25e8713ec472224c0ab7d28401f245a52e58b3
Eli Cho <eli.cho@gmail.com>
2018-07-26 06:59:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,8 @@
 def oxford_comma(array)
-  new_array = array.slice(0...array.length)
-  return new_array.join(", ") + "and #{array.last}"
+  new_array = array.slice(0...(array.length))
+  if array.length == 1
+    return array[0]
+  else
+    return new_array.join(", ") + "and #{array.last}"
+  end
 end
